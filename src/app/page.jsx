6190fe3f74f536cecaaa8ed3abe28a70082ff6a6
hosein mohammadi <hosein011dev@gmail.com>
2025-10-22 20:29:24 +0330

    add sec 3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,47 +26,47 @@ export default function homePage() {
         {/* sec1 */}
         <section className='mt-8 px-[5%] flex justify-center gap-2.5'>
           <div className='w-[950px] rounded-2xl overflow-hidden'>
-            <MySlider/>
+            <MySlider />
           </div>
           <div className='flex flex-col justify-between w-[350px]'>
             <Image src='/image/womenSec.png' alt="Pic" width={350} height={350} className='rounded-2xl' />
-            <Image src='/image/manSec.jpeg' alt="Pic" width={350} height={350}  className='rounded-2xl'/>
+            <Image src='/image/manSec.jpeg' alt="Pic" width={350} height={350} className='rounded-2xl' />
           </div>
         </section>
 
-    
+
         {/* sec2 */}
         <section className='mt-6 px-[7%] flex justify-between *:bg-white *:p-2 *:shadow-lg *:rounded-2xl *:w-2/'>
           <div className='flex gap-3'>
-            <Image src='/icon/poshtibani.png' alt="Pic" width={50} height={50}/>
+            <Image src='/icon/poshtibani.png' alt="Pic" width={50} height={50} />
             <div>
               <p className='text-red-600'>مشاوره خرید به مشتریان</p>
               <p className='text-sm text-black/80'>از 9 صبح تا 17 عصر</p>
             </div>
           </div>
           <div className='flex gap-3'>
-            <Image src='/icon/expres.jpeg' alt="Pic" width={50} height={50}/>
+            <Image src='/icon/expres.jpeg' alt="Pic" width={50} height={50} />
             <div>
               <p className='text-red-600'>ارسال سریع یا اکسپرس</p>
               <p className='text-sm text-black/80'>به سراسر ایران</p>
             </div>
           </div>
           <div className='flex gap-3 w-54'>
-            <Image src='/icon/frosh.png' alt="Pic" width={50} height={50}/>
+            <Image src='/icon/frosh.png' alt="Pic" width={50} height={50} />
             <div>
               <p className='text-red-600'>خدمات پس از فروش</p>
               <p className='text-sm text-black/80'>10 سال</p>
             </div>
           </div>
           <div className='flex gap-3 w-54'>
-            <Image src='/icon/garanti.png' alt="Pic" width={50} height={50}/>
+            <Image src='/icon/garanti.png' alt="Pic" width={50} height={50} />
             <div>
               <p className='text-red-600'>گارانتی باتری</p>
               <p className='text-sm text-black/80'>5 سال ضمانت</p>
             </div>
           </div>
           <div className='flex gap-3'>
-            <Image src='/icon/taviz.png' alt="Pic" width={50} height={50}/>
+            <Image src='/icon/taviz.png' alt="Pic" width={50} height={50} />
             <div>
               <p className='text-red-600'>امکان تعویض</p>
               <p className='text-sm text-black/80'>تا 48 ساعت پس از تحویل</p>
@@ -75,6 +75,12 @@ export default function homePage() {
         </section>
 
 
+        {/* sec3 */}
+        <section className='mt-6 px-[7%]'>
+          <Link href=''>
+            <Image className='rounded-2xl overflow-hidden' src='/image/baner.jpeg' alt="Pic" width={10000} height={50} />
+          </Link>
+        </section>
       </main>
     </>
   )
