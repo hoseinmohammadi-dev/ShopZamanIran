21a4ea0a3e706f3a7868eab346696f632f3849eb
hosein mohammadi <hosein011dev@gmail.com>
2025-10-23 18:56:15 +0330

    responsive footer

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -4,33 +4,31 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaLocationArrow } from "react-icons/fa";
 import Image from 'next/image';
 
-export default function Footer(){
-    return(
-         <footer className='flex items-center mt-8 p-5 bg-stone-200 '>
-          <Image className='rounded-2xl overflow-hidden ml-12' src='/icon/newLogo.png' alt="Pic" width={150} height={50} />
+export default function Footer() {
+  return (
+    <footer className='flex items-center md:items-start mt-8 px-10 py-5 bg-stone-200'>
+      <div className='hidden md:block'>
+        <h4 className='text-[14px] mb-4'>مرجع ساعت های اورجینال ، لوکس و کلکسیونی - زمان ایران</h4>
+        <p className='w-[85%] text-[12px] text-black/75'>فروشگاه آنلاین زمان ایران، در زمینه واردات و فروش اینترنتی انواع ساعت با بیش از 30 سال فعالیت در این حوزه می باشد. فروش انواع ساعت های اورجینال به همراه انواع برند های لوکس ساعت در فروشگاه ساعت زمان ایران ساعت زمان ایران با سابقه ی بیش از 30 سال در این حوضه توانسته با اکثر برند های لوکس ساعت در جهان همکاری داشته باشه علاوه بر این فروشگاه ساعت زمان ایران اکثر محصولات انواع برند های اورجینال مثل کاسیو, تیسوت, سیکو, سیتیزن, امپریو آرمانی, هوگو بوس, فسیل, دنیل کلین را به صورت مستقیم به دست علاقه مندان این محصولات میرساند.</p>
+      </div>
 
-          <div className='w-7/12'>
-            <h4>مرجع ساعت های اورجینال ، لوکس و کلکسیونی - زمان ایران</h4>
-            <p className='text-sm text-black/75 mt-2'>فروشگاه آنلاین زمان ایران، در زمینه واردات و فروش اینترنتی انواع ساعت با بیش از 30 سال فعالیت در این حوزه می باشد. فروش انواع ساعت های اورجینال به همراه انواع برند های لوکس ساعت در فروشگاه ساعت زمان ایران ساعت زمان ایران با سابقه ی بیش از 30 سال در این حوضه توانسته با اکثر برند های لوکس ساعت در جهان همکاری داشته باشه علاوه بر این فروشگاه ساعت زمان ایران اکثر محصولات انواع برند های اورجینال مثل کاسیو, تیسوت, سیکو, سیتیزن, امپریو آرمانی, هوگو بوس, فسیل, دنیل کلین را به صورت مستقیم به دست علاقه مندان این محصولات میرساند.</p>
+      <div className=''>
+        <p className='text-[14px] mb-4'>پشتیانی از 9 صبح الی 18 عصر</p>
+        <div className='*:text-[12px] *:mt-1'>
+          <div className='flex items-center gap-1.5'>
+            <FaPhone className='text-stone-500' />
+            <p>09129272196</p>
           </div>
-
-          <div className='w-5/12 mr-20'>
-            <p>پشتیانی از 9 صبح الی 18 عصر</p>
-            <div className='*:text-sm *:mt-1'>
-            <div className='flex items-center gap-1.5'>
-              <FaPhone className='text-stone-500'/>
-              <p>09129272196</p>
-            </div>
-            <div className='flex items-center gap-1.5'>
-              <MdOutlineMail className='text-stone-500'/>
-              <p>Info@Zamaniran.com</p>
-            </div>
-            <div className='flex gap-1.5 relative'>
-              <FaLocationArrow  className='text-stone-500 relative top-2'/>
-              <p className='w-[270px]'>تهران – اتوبان ستاری – مجتمع تجاری کوروش – طبقه منفی یک – واحد 166 - گالری زمان ایران</p>
-            </div>
-            </div>
+          <div className='flex items-center gap-1.5'>
+            <MdOutlineMail size={15} className='text-stone-500' />
+            <p>Info@Zamaniran.com</p>
           </div>
-        </footer>
-    )
+          <div className='flex gap-1.5 relative w-[220px]'>
+            <FaLocationArrow size={20} className='text-stone-500 relative top-0' />
+            <p>تهران – اتوبان ستاری – مجتمع تجاری کوروش – طبقه منفی یک – واحد 166 - گالری زمان ایران</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
