f1c1941d045481a8c3cfaacbd568f8986d94a7ac
hosein mohammadi <hosein011dev@gmail.com>
2025-10-25 18:01:43 +0330

    fixed bug

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className='bg-stone-100 overflow-hidden'>
+      <body className='bg-stone-100'>
         <Header/>
         {children}
         <Footer/>
